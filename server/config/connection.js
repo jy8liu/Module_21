3edfefb0af6d06d0c34b45e5603f26fad4ebf1df
jy8liu <jy8liu@gmail.com>
2024-09-29 21:25:35 -0700

    it works!

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-shopping');
+mongoose.connect('mongodb+srv://jy8liu:hgnXDtKrBOBCkMs2@cluster0.ebesr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
 module.exports = mongoose.connection;
